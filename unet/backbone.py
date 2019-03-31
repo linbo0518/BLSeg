3d7ee3efa3b365d226f571d2474e5b1fd1612680
linbo0518 <linbo0518@gmail.com>
2019-03-31 20:23:28 +0800

    refactor self._add_stage to make it more flex

--- unet/backbone.py
+++ unet/backbone.py
@@ -12,11 +12,13 @@ class ResNet34(nn.Module):
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
+            # nn.MaxPool2d(3, stride=2, padding=1),
+            # remove maxpool to keep featuremap size not too small
         )
-        self.stage1 = self._add_stage(ResidualBlock, 64, 3)
-        self.stage2 = self._add_stage(ResidualBlock, 128, 4)
-        self.stage3 = self._add_stage(ResidualBlock, 256, 6)
-        self.stage4 = self._add_stage(ResidualBlock, 512, 3)
+        self.stage1 = self._add_stage(ResidualBlock, 64, 64, 1, 3)
+        self.stage2 = self._add_stage(ResidualBlock, 64, 128, 2, 4)
+        self.stage3 = self._add_stage(ResidualBlock, 128, 256, 2, 6)
+        self.stage4 = self._add_stage(ResidualBlock, 256, 512, 2, 3)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -34,14 +36,9 @@ class ResNet34(nn.Module):
         x = self.stage4(x)
         return x
 
-    def _add_stage(self, block, channel, repeat):
-        block_list = []
-        stride = 1
-        for _ in range(repeat):
-            if self.channel != channel:
-                stride = 2
-            else:
-                stride = 1
-            block_list.append(block(self.channel, channel, stride))
-            self.channel = channel
-        return nn.Sequential(*block_list)
+    def _add_stage(self, block, in_ch, out_ch, stride, repeat_times):
+        assert repeat_times > 0 and isinstance(repeat_times, int)
+        layers = [block(in_ch, out_ch, stride)]
+        for _ in range(repeat_times - 1):
+            layers.append(block(out_ch, out_ch, 1))
+        return nn.Sequential(*layers)
