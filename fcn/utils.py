0e2af44d032c98432d4a7f66757cab1a94227a42
linbo0518 <linbo0518@gmail.com>
2019-04-07 22:49:07 +0800

    add depthwiseseparableconv

--- fcn/utils.py
+++ fcn/utils.py
@@ -6,6 +6,26 @@ def conv3x3(in_ch, out_ch, stride=1):
     return nn.Conv2d(in_ch, out_ch, 3, stride=stride, padding=1, bias=False)
 
 
+class DepthwiseSeparableConv(nn.Module):
+
+    def __init__(self, in_ch, out_ch, stride=1):
+        super(DepthwiseSeparableConv, self).__init__()
+        self.dwconv = nn.Conv2d(
+            in_ch, in_ch, 3, stride=stride, padding=1, groups=in_ch, bias=False)
+        self.bn1 = nn.BatchNorm2d(in_ch)
+        self.pwconv = nn.Conv2d(in_ch, out_ch, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_ch)
+        self.relu6 = nn.ReLU6(inplace=True)
+
+    def forward(self, x):
+        x = self.dwconv(x)
+        x = self.bn1(x)
+        x = self.relu6(x)
+        x = self.pwconv(x)
+        x = self.bn2(x)
+        return self.relu6(x)
+
+
 class ResidualBlock(nn.Module):
 
     def __init__(self, in_ch, out_ch, stride):
