0c8731b2bbb837a7e72bf38a9ab06dfa827cf71a
linbo0518 <linbo0518@gmail.com>
2019-04-10 16:36:18 +0800

    unet reborn

--- llseg/unet/unet.py
+++ llseg/unet/unet.py
@@ -1,16 +1,16 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
-from .utils import conv3x3, DownBlock, UpBlock, ResidualBlock, DecodeBlock
-from .backbone import ResNet34
+from ..backbone import VGG16, MobileNetV1, ResNet50S
+from ..backbone.utils import conv3x3
+from .utils import DownBlock, UpBlock, UpConv
 
 
 class UNet(nn.Module):
 
-    def __init__(self, in_ch=3, num_classes=1):
+    def __init__(self, num_classes=1):
         super(UNet, self).__init__()
         self.inputs = nn.Sequential(
-            conv3x3(in_ch, 64),
+            conv3x3(3, 64),
             nn.ReLU(inplace=True),
             conv3x3(64, 64),
             nn.ReLU(inplace=True),
@@ -25,11 +25,7 @@ class UNet(nn.Module):
         self.up_block1 = UpBlock(128, 64)
         self.outputs = nn.Conv2d(64, num_classes, 1, bias=False)
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight,
-                                        mode='fan_out',
-                                        nonlinearity='relu')
+        self._init_params()
 
     def forward(self, x):
         e1 = self.inputs(x)
@@ -43,19 +39,54 @@ class UNet(nn.Module):
         d1 = self.up_block1(e1, d2)
         return self.outputs(d1)
 
+    def _init_params(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight,
+                                        mode='fan_out',
+                                        nonlinearity='relu')
 
-class ResUNet(nn.Module):
 
-    def __init__(self, num_classes=1):
-        super(ResUNet, self).__init__()
-        self.encoder = ResNet34()
-        self.center = ResidualBlock(512, 1024, 2)
-        self.decoder4 = DecodeBlock(1024, 512)
-        self.decoder3 = DecodeBlock(512, 256)
-        self.decoder2 = DecodeBlock(256, 128)
-        self.decoder1 = DecodeBlock(128, 64)
-        self.outputs = nn.Conv2d(64, num_classes, 1, bias=False)
+class ModernUNet(nn.Module):
 
+    def __init__(self, backbone='resnet50', num_classes=1):
+        assert backbone in ['vgg16', 'resnet50', 'mobilenetv1']
+        super(ModernUNet, self).__init__()
+        if backbone == 'vgg16':
+            self.backbone = VGG16()
+        elif backbone == 'resnet50':
+            self.backbone = ResNet50S()
+        elif backbone == 'mobilenetv1':
+            self.backbone = MobileNetV1()
+
+        self.up_block4 = UpBlock(self.backbone.channels[4],
+                                 self.backbone.channels[3])
+        self.up_block3 = UpBlock(self.backbone.channels[3],
+                                 self.backbone.channels[2])
+        self.up_block2 = UpBlock(self.backbone.channels[2],
+                                 self.backbone.channels[1])
+        self.up_block1 = UpBlock(self.backbone.channels[1],
+                                 self.backbone.channels[0])
+        self.outputs = nn.Sequential(
+            UpConv(self.backbone.channels[0], self.backbone.channels[0]),
+            nn.Conv2d(self.backbone.channels[0], num_classes, 1, bias=False),
+        )
+
+        self._init_params()
+
+    def forward(self, x):
+        e1 = self.backbone.stage0(x)
+        e2 = self.backbone.stage1(e1)
+        e3 = self.backbone.stage2(e2)
+        e4 = self.backbone.stage3(e3)
+        e5 = self.backbone.stage4(e4)
+        d4 = self.up_block4(e4, e5)
+        d3 = self.up_block3(e3, d4)
+        d2 = self.up_block2(e2, d3)
+        d1 = self.up_block1(e1, d2)
+        return self.outputs(d1)
+
+    def _init_params(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight,
@@ -64,20 +95,3 @@ class ResUNet(nn.Module):
             if isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
-
-    def forward(self, x):
-        e0 = self.encoder.stage0(x)
-        e1 = self.encoder.stage1(e0)
-        e2 = self.encoder.stage2(e1)
-        e3 = self.encoder.stage3(e2)
-        e4 = self.encoder.stage4(e3)
-        e5 = self.center(e4)
-        d4 = self.decoder4(e4, e5)
-        d3 = self.decoder3(e3, d4)
-        d2 = self.decoder2(e2, d3)
-        d1 = self.decoder1(e1, d2)
-        d1 = F.interpolate(d1,
-                           scale_factor=2,
-                           mode='bilinear',
-                           align_corners=False)
-        return self.outputs(d1)
--- llseg/unet/utils.py
+++ llseg/unet/utils.py
@@ -1,10 +1,7 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
-
-
-def conv3x3(in_ch, out_ch, stride=1):
-    return nn.Conv2d(in_ch, out_ch, 3, stride=stride, padding=1, bias=False)
+from torch.nn import functional as F
+from ..backbone.utils import conv3x3, ResidualBlock
 
 
 class DownBlock(nn.Module):
@@ -28,13 +25,14 @@ class UpConv(nn.Module):
     def __init__(self, in_ch, out_ch, scale_factor=2):
         super(UpConv, self).__init__()
         self.scale_factor = scale_factor
-        self.conv = conv3x3(in_ch, out_ch)
+        self.conv = nn.Conv2d(in_ch, out_ch, 2, bias=False)
 
     def forward(self, x):
         x = F.interpolate(x,
                           scale_factor=self.scale_factor,
                           mode='bilinear',
                           align_corners=False)
+        x = F.pad(x, (0, 1, 0, 1))
         return self.conv(x)
 
 
@@ -44,7 +42,7 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         self.up_conv = UpConv(in_ch, out_ch)
         self.up_block = nn.Sequential(
-            conv3x3(in_ch, out_ch),
+            conv3x3(out_ch * 2, out_ch),
             nn.ReLU(inplace=True),
             conv3x3(out_ch, out_ch),
             nn.ReLU(inplace=True),
@@ -54,49 +52,3 @@ class UpBlock(nn.Module):
         x = self.up_conv(x)
         x = torch.cat((encoded, x), dim=1)
         return self.up_block(x)
-
-
-class ResidualBlock(nn.Module):
-
-    def __init__(self, in_ch, out_ch, stride):
-        super(ResidualBlock, self).__init__()
-        self.do_downsample = not (in_ch == out_ch and stride == 1)
-        self.conv1 = conv3x3(in_ch, out_ch, stride)
-        self.bn1 = nn.BatchNorm2d(out_ch)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(out_ch, out_ch)
-        self.bn2 = nn.BatchNorm2d(out_ch)
-
-        if self.do_downsample:
-            self.conv3 = nn.Conv2d(in_ch, out_ch, 1, stride, bias=False)
-            self.bn3 = nn.BatchNorm2d(out_ch)
-
-    def forward(self, x):
-        residual = x
-
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-
-        if self.do_downsample:
-            residual = self.conv3(residual)
-            residual = self.bn3(residual)
-
-        x += residual
-        x = self.relu(x)
-        return x
-
-
-class DecodeBlock(nn.Module):
-
-    def __init__(self, in_ch, out_ch):
-        super(DecodeBlock, self).__init__()
-        self.up_conv = UpConv(in_ch, out_ch)
-        self.decode_block = ResidualBlock(in_ch, out_ch, 1)
-
-    def forward(self, encoded, x):
-        x = self.up_conv(x)
-        x = torch.cat((encoded, x), dim=1)
-        return self.decode_block(x)
