a5eb84c0e85399a7062a0d71a8d2ab08afc8b18f
linbo0518 <linbo0518@gmail.com>
2019-04-19 17:51:20 +0800

    add conv bias initializer

--- blseg/backbone/base.py
+++ blseg/backbone/base.py
@@ -14,6 +14,8 @@ class BackboneBaseModule(nn.Module):
                 nn.init.kaiming_normal_(m.weight,
                                         mode='fan_out',
                                         nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
             if isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
