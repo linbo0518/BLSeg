ca24c9e91fc2686432bf331e1561357043d0691a
linbo0518 <linbo0518@gmail.com>
2019-04-25 16:39:17 +0800

    add ignore_background feature

--- blseg/metric/metric.py
+++ blseg/metric/metric.py
@@ -26,10 +26,11 @@ class PixelAccuracy(object):
 
 class MeanIoU(object):
 
-    def __init__(self, num_classes, eps=1e-12):
+    def __init__(self, num_classes, ignore_background=False, eps=1e-12):
         self.num_classes = num_classes
         self.num_intersection = np.zeros(self.num_classes)
         self.num_union = np.zeros(self.num_classes)
+        self.ignore_background = ignore_background
         self.eps = eps
 
     def update(self, pred, target):
@@ -44,8 +45,11 @@ class MeanIoU(object):
             self.num_union[cur_cls] += union.item()
 
     def get(self):
-        return (self.num_intersection[1:] /
-                (self.num_union[1:] + self.eps)).mean()
+        if self.ignore_background:
+            return (self.num_intersection[1:] /
+                    (self.num_union[1:] + self.eps)).mean()
+        else:
+            return (self.num_intersection / (self.num_union + self.eps)).mean()
 
     def reset(self):
         self.num_intersection = np.zeros(self.num_classes)
