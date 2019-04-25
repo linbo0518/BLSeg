9ea9b65abfb1d437876b31a9bb28567a7bfd5eae
linbo0518 <linbo0518@gmail.com>
2019-04-25 13:34:05 +0800

    add load_parameters

--- blseg/backbone/base.py
+++ blseg/backbone/base.py
@@ -8,23 +8,10 @@ class BackboneBaseModule(nn.Module):
         super(BackboneBaseModule, self).__init__()
         self.strides = [2, 4, 8, 16, 32]
 
-    def _init_params(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight,
-                                        mode='fan_out',
-                                        nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            if isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-    def _change_downsample(self, params):
-        '''
-        Should be overridden by all subclasses
-        '''
-        raise NotImplementedError
+    def load_parameters(self, filename, map_location=None, strict=True):
+        # TODO: waiting for test
+        self.load_state_dict(torch.load(filename, map_location=map_location),
+                             strict=strict)
 
     def change_output_stride(self, output_stride):
         assert output_stride in [8, 16, 32]
@@ -41,13 +28,6 @@ class BackboneBaseModule(nn.Module):
             self.strides[3] = 8
             self.strides[4] = 8
 
-    def _change_stage_dilation(self, stage, param):
-        for m in stage.modules():
-            if isinstance(m, nn.Conv2d):
-                if m.kernel_size == (3, 3):
-                    m.padding = (param, param)
-                    m.dilation = (param, param)
-
     def change_dilation(self, params):
         assert isinstance(params, list)
         assert len(params) == 5
@@ -56,3 +36,28 @@ class BackboneBaseModule(nn.Module):
         self._change_stage_dilation(self.stage2, params[2])
         self._change_stage_dilation(self.stage3, params[3])
         self._change_stage_dilation(self.stage4, params[4])
+
+    def _change_stage_dilation(self, stage, param):
+        for m in stage.modules():
+            if isinstance(m, nn.Conv2d):
+                if m.kernel_size == (3, 3):
+                    m.padding = (param, param)
+                    m.dilation = (param, param)
+
+    def _change_downsample(self, params):
+        '''
+        Should be overridden by all subclasses
+        '''
+        raise NotImplementedError
+
+    def _init_params(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight,
+                                        mode='fan_out',
+                                        nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            if isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
