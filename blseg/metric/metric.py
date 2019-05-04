588e210c23a340f8e001171f93ca37b007322679
linbo0518 <linbo0518@gmail.com>
2019-05-04 22:06:29 +0800

    fix MeanIoU's bug in binary classification

--- blseg/metric/metric.py
+++ blseg/metric/metric.py
@@ -27,20 +27,27 @@ class PixelAccuracy(object):
 class MeanIoU(object):
 
     def __init__(self, num_classes, ignore_background=False, eps=1e-7):
-        self.num_classes = num_classes
+        if num_classes == 1:
+            self.num_classes = num_classes + 1
+        else:
+            self.num_classes = num_classes
         self.num_intersection = np.zeros(self.num_classes)
         self.num_union = np.zeros(self.num_classes)
         self.ignore_background = ignore_background
         self.eps = eps
 
     def update(self, pred, target):
-        pred = torch.argmax(pred, dim=1)
+        if pred.size(1) == 1:
+            pred = torch.sigmoid(pred)
+            pred = pred > 0.5
+        else:
+            pred = torch.argmax(pred, dim=1)
         for cur_cls in range(self.num_classes):
             pred_mask = (pred == cur_cls).byte()
             target_mask = (target == cur_cls).byte()
 
-            intersection = (pred_mask & target_mask).float().sum((0, 1, 2))
-            union = (pred_mask | target_mask).float().sum((0, 1, 2))
+            intersection = (pred_mask & target_mask).float().sum()
+            union = (pred_mask | target_mask).float().sum()
             self.num_intersection[cur_cls] += intersection.item()
             self.num_union[cur_cls] += union.item()
 
